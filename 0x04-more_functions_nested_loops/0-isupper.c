a21c5df2a88584765ca3d7627382bd684a153966
PK Blankson <blankson8@gmail.com>
2022-10-20 06:35:40 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,16 @@
 /**
  * _isupper - checks if a character is uppercase or not
  * @c: character to be checked
+ *
  * Return: return 1 if uppercase or 0 if otherwise
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
+
 	return (0);
 }
